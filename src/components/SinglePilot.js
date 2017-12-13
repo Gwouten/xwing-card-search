039dd04b07259835870ae4fca3187ac76ec6c310
Gwouten <wouter.deloof@gmail.com>
2017-12-13 18:09:24 +0100

    Start new styling

--- src/components/SinglePilot.js
+++ src/components/SinglePilot.js
@@ -3,13 +3,16 @@ import convertToIcon from '../helpers/convertToIcon';
 
 const SinglePilot = ({ faction, id, image, name, points, ship, skill, slots, text, unique, xws }) => (
   <div className={`cardlist__card  ${faction.split(' ')[0]}`}>
-    <h3>{unique && "•"} {name} - {points}</h3>
-    <h4>Pilot Skill: {skill} - {ship}</h4>
+    <h3 className="cardlist__card__title">{unique && "•"} {name}</h3>
+    <h4 className="cardlist__card__subtitle">Pilot Skill: {skill} - {ship}</h4>
     <p>{text ? text : ''}</p>
-    <ul>Upgrades: { slots.map((slot, index) => (
-      <li key={id+index} className="xwing-miniatures-font">{convertToIcon([slot])}</li>
-      )) }
-    </ul>
+    <div className="cardlist__card__footer">
+      <ul className="cardlist__card__icons">{ slots.map((slot, index) => (
+        <li key={id+index} className="cardlist__card__icons__icon xwing-miniatures-font">{convertToIcon([slot])}</li>
+        )) }
+      </ul>
+      <p className="cardlist__footer__points">{points} pts</p>
+    </div>
   </div>
 );
 
