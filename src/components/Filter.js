73fd93f3ee72ff4b817ffc9672d4ce96718b8fd8
Gwouten <wouter.deloof@gmail.com>
2017-12-21 11:56:18 +0100

    Add overlay that toggles filter menu

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -65,105 +65,108 @@ class Filter extends React.Component {
 
   render(){
     return (
-      <div className={`filter content-container ${this.props.meta.filterMenuHidden && 'filter--hidden'}`}>
-        <form onSubmit={this.onSubmit}>
-          <div className="filter__text__search">
-            <input className="filter__input" type="text" name="textQuery" placeholder="Search" />
-            <button className="filter__text__search__button button" onClick={this.handleClear}>Clear</button>
-          </div>
+      <div>
+        <div className={`filter__background ${this.props.meta.filterMenuHidden && 'filter__background--hidden'}`} onClick={this.toggleFilterMenu}></div>
+        <div className={`filter content-container ${this.props.meta.filterMenuHidden && 'filter--hidden'}`}>
+          <form onSubmit={this.onSubmit}>
+            <div className="filter__text__search">
+              <input className="filter__input" type="text" name="textQuery" placeholder="Search" />
+              <button className="filter__text__search__button button" onClick={this.handleClear}>Clear</button>
+            </div>
 
-          <div className="filter__wrapper">
-            <h2 className="filter__title">Faction</h2>
-            <input
-              className="filter__faction__search__radio-button"
-              type="radio"
-              id="faction_any"
-              name="faction"
-              value="any"
-              onChange={this.onFactionSelect}
-              checked={this.props.filters.faction === "any"} />
-            <label className="filter__label" htmlFor="faction_any">Any</label>
-            <input
-              className="filter__faction__search__radio-button"
-              type="radio"
-              id="faction_rebel"
-              name="faction"
-              value="rebel"
-              onChange={this.onFactionSelect}
-              checked={this.props.filters.faction === "rebel"} />
-            <label className="filter__label" htmlFor="faction_rebel">Rebel</label>
-            <input
-              className="filter__faction__search__radio-button"
-              type="radio"
-              id="faction_empire"
-              name="faction"
-              value="empire"
-              onChange={this.onFactionSelect}
-              checked={this.props.filters.faction === "empire"} />
-            <label className="filter__label" htmlFor="faction_empire">Empire</label>
-            <input
-              className="filter__faction__search__radio-button"
-              type="radio"
-              id="faction_scum"
-              name="faction"
-              value="scum"
-              onChange={this.onFactionSelect}
-              checked={this.props.filters.faction === "scum"} />
-            <label className="filter__label" htmlFor="faction_scum">Scum</label>
-          </div>
-
-          <div className="filter__wrapper">
-            <h2 className="filter__title">Card Type</h2>
-            <input
-              className="filter__card__type__checkbox"
-              type="checkbox"
-              id="pilot"
-              name="pilot"
-              value="pilot"
-              checked={this.props.filters.cardTypePilot}
-              onChange={this.onPilotSelected} />
-            <label className="filter__label filter__label--checkbox" htmlFor="pilot">Pilot</label>
-            <input
-              className="filter__card__type__checkbox"
-              type="checkbox"
-              id="upgrade"
-              name="upgrade"
-              value="upgrade"
-              checked={this.props.filters.cardTypeUpgrade}
-              onChange={this.onUpgradeSelected} />
-            <label className="filter__label filter__label--checkbox" htmlFor="upgrade">Upgrade</label>
-          </div>
-
-          <div className="filter__points">
-            <h2 className="filter__title">Points Range</h2>
-            <p className="filter__points__text">Show cards with a cost between</p>
-            <p className="filter__points__text">
+            <div className="filter__wrapper">
+              <h2 className="filter__title">Faction</h2>
               <input
-                className="filter__input filter__input--points"
-                name="minPoints"
-                type="number"
-                min="-3"
-                max="100"
-                step="1"
-                onChange={this.onSetMinPoints}
-                onFocus={this.onMinPointsFocus}
-                value={this.props.filters.minPoints} />
-             and
+                className="filter__faction__search__radio-button"
+                type="radio"
+                id="faction_any"
+                name="faction"
+                value="any"
+                onChange={this.onFactionSelect}
+                checked={this.props.filters.faction === "any"} />
+              <label className="filter__label" htmlFor="faction_any">Any</label>
               <input
-                className="filter__input filter__input--points"
-                name="maxPoints" type="number"
-                min="-3"
-                max="100"
-                step="1"
-                onChange={this.onSetMaxPoints}
-                onFocus={this.onMaxPointsFocus}
-                value={this.props.filters.maxPoints} />
-            </p>
-            {/* <button className="filter__points__button button" onClick={this.clearPoints}>Reset points range</button> */}
-            <button className="filter__points__button button">Search</button>
-          </div>
-        </form>
-        <button className="filter__close" onClick={this.toggleFilterMenu}>X</button>
+                className="filter__faction__search__radio-button"
+                type="radio"
+                id="faction_rebel"
+                name="faction"
+                value="rebel"
+                onChange={this.onFactionSelect}
+                checked={this.props.filters.faction === "rebel"} />
+              <label className="filter__label" htmlFor="faction_rebel">Rebel</label>
+              <input
+                className="filter__faction__search__radio-button"
+                type="radio"
+                id="faction_empire"
+                name="faction"
+                value="empire"
+                onChange={this.onFactionSelect}
+                checked={this.props.filters.faction === "empire"} />
+              <label className="filter__label" htmlFor="faction_empire">Empire</label>
+              <input
+                className="filter__faction__search__radio-button"
+                type="radio"
+                id="faction_scum"
+                name="faction"
+                value="scum"
+                onChange={this.onFactionSelect}
+                checked={this.props.filters.faction === "scum"} />
+              <label className="filter__label" htmlFor="faction_scum">Scum</label>
+            </div>
+
+            <div className="filter__wrapper">
+              <h2 className="filter__title">Card Type</h2>
+              <input
+                className="filter__card__type__checkbox"
+                type="checkbox"
+                id="pilot"
+                name="pilot"
+                value="pilot"
+                checked={this.props.filters.cardTypePilot}
+                onChange={this.onPilotSelected} />
+              <label className="filter__label filter__label--checkbox" htmlFor="pilot">Pilot</label>
+              <input
+                className="filter__card__type__checkbox"
+                type="checkbox"
+                id="upgrade"
+                name="upgrade"
+                value="upgrade"
+                checked={this.props.filters.cardTypeUpgrade}
+                onChange={this.onUpgradeSelected} />
+              <label className="filter__label filter__label--checkbox" htmlFor="upgrade">Upgrade</label>
+            </div>
+
+            <div className="filter__points">
+              <h2 className="filter__title">Points Range</h2>
+              <p className="filter__points__text">Show cards with a cost between</p>
+              <p className="filter__points__text">
+                <input
+                  className="filter__input filter__input--points"
+                  name="minPoints"
+                  type="number"
+                  min="-3"
+                  max="100"
+                  step="1"
+                  onChange={this.onSetMinPoints}
+                  onFocus={this.onMinPointsFocus}
+                  value={this.props.filters.minPoints} />
+               and
+                <input
+                  className="filter__input filter__input--points"
+                  name="maxPoints" type="number"
+                  min="-3"
+                  max="100"
+                  step="1"
+                  onChange={this.onSetMaxPoints}
+                  onFocus={this.onMaxPointsFocus}
+                  value={this.props.filters.maxPoints} />
+              </p>
+              {/* <button className="filter__points__button button" onClick={this.clearPoints}>Reset points range</button> */}
+              <button className="filter__points__button button">Search</button>
+            </div>
+          </form>
+          <button className="filter__close" onClick={this.toggleFilterMenu}>X</button>
+        </div>
       </div>
     );
   }
